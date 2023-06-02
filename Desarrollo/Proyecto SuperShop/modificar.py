bd53c52fad67c8ce672a21f1885a9949f3b926b2
bitzzz <santacruzedward@hotmail.com>
2023-06-01 20:14:52 -0500

    Arreglo de funcion Modificar e insercion del documento SS-DVSI2

--- Desarrollo/Proyecto SuperShop/modificar.py
+++ Desarrollo/Proyecto SuperShop/modificar.py
@@ -13,13 +13,17 @@ back = os.path.join(BASE_DIR, "Resources\\fondo.png")
 cuadro_Trans = os.path.join(BASE_DIR, "Resources\\cuadroTrans.png")
 logo_SS = os.path.join(BASE_DIR, "Resources\\SS_Logo.png")
 
-app = QApplication([])
+#app = QApplication([])
 
 class VentanaModificar(QMainWindow):
     def __init__(self, parent = None, *args):
         super(VentanaModificar, self).__init__(parent = None)
 
         self.Id = None
+        self.Precio = None
+        self.Nombre = None
+        self.Desc = None
+        self.Stock = None
 
         self.setFont(QFont('arial', 20))
         self.setFixedSize(1280,720)
@@ -40,28 +44,58 @@ class VentanaModificar(QMainWindow):
         logoSS.setGeometry(200,40,logoSS1.width(),logoSS1.height())
         logoSS.setPixmap(logoSS1)
 
-        texto = QLabel("Ingresa la ID del producto a modificar: ", self)
-        texto.setGeometry(100, 220, 600, 100)
+        texto = QLabel("Ingrese la ID del producto a modificar: ", self)
+        texto.setGeometry(150, 230, 400, 100)
 
         label = QLabel("ID: ", self)
         label.setGeometry(250, 280, 100, 100)
 
+        label1 = QLabel("Precio: ", self)
+        label1.setGeometry(600, 280, 100, 100)
+
+        label2 = QLabel("Nombre: ", self)
+        label2.setGeometry(250, 380, 100, 100)
+
+        label3 = QLabel("Descripcion: ", self)
+        label3.setGeometry(600, 380, 300, 100)
+
+        label4 = QLabel("Stock: ", self)
+        label4.setGeometry(250, 480, 100, 100)
+
         self.inputID = QLineEdit(self)
         self.inputID.setGeometry(250, 355, 300, 45)
         self.inputID.setClearButtonEnabled(True)
-        self.inputID.returnPressed.connect(self.show_text)
+
+        self.inputPrecio = QLineEdit(self)
+        self.inputPrecio.setGeometry(600, 355, 300, 45)
+        self.inputPrecio.setClearButtonEnabled(True)
+
+        self.inputNombre = QLabel(self. Nombre, self)
+        self.inputNombre.setGeometry(250, 455, 300, 45)
+
+        self.inputDesc = QLabel(self.Desc ,self)
+        self.inputDesc.setGeometry(600, 455, 300, 145)
+
+        self.inputStock = QLineEdit(self)
+        self.inputStock.setGeometry(250, 555, 300, 45)
+        self.inputStock.setClearButtonEnabled(True)
 
         self.btn = QPushButton("Modificar", self)
         self.btn.setGeometry(950, 455, 150, 50)
-        self.btn.clicked.connect(lambda: self.eliminar())
-        
+        self.btn.clicked.connect(lambda: self.modificar())
+
         self.btn2 = QPushButton("<<", self)
         self.btn2.setGeometry(50, 50, 60, 60)
         self.btn2.clicked.connect(lambda: self.AbrirMenu())
 
-
-    def show_text(self):
+    def modificar(self):
         self.Id = self.inputID.text()
+        self.Precio = self.inputPrecio.text()
+        self.Stock = self.inputStock.text()
+        parametros = (self.Stock, self.Precio, self.Id)
+        query = "UPDATE Producto SET stock = ?, precio = ? WHERE id = ?"
+        self.consultar(query, parametros)
+        QMessageBox.question(self, 'LISTO', "El producto se modifico correctamente de la base de datos", QMessageBox.Ok)
 
     def consultar(self, query, parameters = ()):
         with sql.connect(db_path) as conn:
@@ -71,13 +105,13 @@ class VentanaModificar(QMainWindow):
             conn.commit()
         return result
 
-    def eliminar(self):
-        query = 'DELETE FROM Producto WHERE Id = ?'
-        parametros = (self.Id,)
-        self.consultar(query, parametros)
-        QMessageBox.question(self, 'LISTO', "El producto se elimino correctamente de la base de datos", QMessageBox.Ok)
-
     def AbrirMenu(self):
         self.close()
         window = Func.VentanaMenu()
         window.show()
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = VentanaModificar()
+    window.show()
+    app.exec_()
